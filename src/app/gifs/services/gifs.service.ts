57d6d0262757b51a291ee7f9b9f648f864458d13
wnicolas <nicolas.buitrago2@gmail.com>
2022-07-07 18:13:52 -0500

    HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGIFResponse } from '../interface/gif.interface';
 
@@ -7,13 +7,15 @@ import { Gif, SearchGIFResponse } from '../interface/gif.interface';
   providedIn: 'root',
 })
 export class GifsService {
-  private apiKey: string = ' ';
+  private apiKey: string = '19xNlbVfq0GKCG4x9uK8DOUlbEV7NwVf';
+  private servicioURL: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
   public resultado: Gif[] = [];
 
   constructor(private http: HttpClient) {
     this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultado = JSON.parse(localStorage.getItem('resultado')!) || [];
   }
 
   get historial() {
@@ -28,15 +30,18 @@ export class GifsService {
 
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
-    console.log(this._historial);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', query)
+      .set('limit', '10');
 
     this.http
-      .get<SearchGIFResponse>(
-        `https://api.giphy.com/v1/gifs/search?api_key=19xNlbVfq0GKCG4x9uK8DOUlbEV7NwVf&q=${query}&limit=10`
-      )
+      .get<SearchGIFResponse>(`${this.servicioURL}/search`, { params: params })
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultado = resp.data;
+        localStorage.setItem('resultado', JSON.stringify(this.resultado));
       });
   }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -12,4 +12,8 @@ export class SidebarComponent {
   }
 
   constructor(private gifsService: GifsService) {}
+
+  buscar(item: string) {
+    this.gifsService.buscarGifs(item);
+  }
 }
